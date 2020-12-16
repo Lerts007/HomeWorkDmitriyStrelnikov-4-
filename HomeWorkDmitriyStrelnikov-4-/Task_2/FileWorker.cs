7036de07f7dcb7caabc1a230a4b187659adfa3db
Lerts <oddff@mail.ru>
2020-12-16 22:49:03 +0300

    Задание 2

---
+++ HomeWorkDmitriyStrelnikov-4-/Task_2/FileWorker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace Task_2
+{
+    public class FileWorker
+    {
+        string path;
+        MatrixWorker mw;
+
+        public FileWorker(string path)
+        {
+            this.path = path;
+            ReadByFile();
+        }
+
+        public void ReadByFile()
+        {
+            StreamReader sr = new StreamReader(path);
+            string t = String.Empty;
+
+            var arg = sr.ReadLine().Split(' ');
+
+            mw = new MatrixWorker(row: int.Parse(arg[0]), int.Parse(arg[1]));
+
+            int x = 0;
+            int y = 0;
+
+            while (!sr.EndOfStream)
+            {
+                t = sr.ReadLine();
+                while (t != "")
+                {
+                    t += " ";
+
+                    int pos = t.IndexOf(' ');
+                    int num = Convert.ToInt32(t.Substring(0, pos));
+
+                    mw.SetElemetByIndex(num, x, y);
+                    y++;
+
+                    t = t.Substring(pos).Trim();
+                }
+                x++;
+                y = 0;
+
+            }
+            sr.Close();
+        }
+
+        public void Print()
+        {
+            mw.Print();
+        }
+    }
+}
---
+++ HomeWorkDmitriyStrelnikov-4-/Task_2/MatrixWorker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Task_2
+{
+    public class MatrixWorker
+    {
+        int[,] matrix;
+        int matrixRow;
+        int matrixCol;
+        public MatrixWorker(int row, int col)
+        {
+            matrixRow = row;
+            matrixCol = col;
+
+            matrix = new int[row, col];
+        }
+        public void SetElemetByIndex(int value, int row, int col)
+        {
+            matrix[row, col] = value;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < matrixRow; i++)
+            {
+                for (int j = 0; j < matrixCol; j++)
+                {
+                    Console.Write($"{matrix[i, j],3}");
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
